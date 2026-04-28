afcaee79a4bd09b09a978824f17026efccbc9fc0
Shxrvxshar7 <arsharvesh7@gmail.com>
2026-04-28 22:32:21 +0530

    feat(arrays): longest consecutive sequence solution

---
+++ arrays/longest_consecutive.py
@@ -0,0 +1,19 @@
+
+
+class Solution(object):
+    def longestConsecutive(self, nums):
+        num_set = set(nums)
+        longest = 0
+
+        for num in num_set:
+            if (num-1) not in num_set:
+                length = 1
+                while(num + length) in num_set:
+                    length += 1
+                longest = max(length, longest)
+        return longest
+
+
+
+        
+# Time complexity: O(n)
