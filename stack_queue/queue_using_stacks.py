7e5a9e30a119affebbd9d51693cce608a87affd0
Shxrvxshar7 <arsharvesh7@gmail.com>
2026-05-04 22:11:12 +0530

    feat(queue): add implement queue using stack solution

---
+++ stack_queue/queue_using_stacks.py
@@ -0,0 +1,40 @@
+# Problem: Implement queue using stack
+# Link: https://leetcode.com/problems/implement-queue-using-stacks/
+# Difficulty: easy
+# Approach: Queue
+
+
+class MyQueue(object):
+
+    def __init__(self):
+        self.stack = []
+        self.stack2 = []
+
+    def push(self, x):
+        self.stack.append(x)
+        
+
+    def pop(self):
+        if not self.stack2:
+            while self.stack:
+                self.stack2.append(self.stack.pop())
+        return self.stack2.pop()
+
+    def peek(self):
+        if not self.stack2:
+            while self.stack:
+                self.stack2.append(self.stack.pop())
+        return self.stack2[-1]
+        
+
+    def empty(self):
+        return max(len(self.stack), len(self.stack2)) == 0
+        
+
+
+# Your MyQueue object will be instantiated and called as such:
+# obj = MyQueue()
+# obj.push(x)
+# param_2 = obj.pop()
+# param_3 = obj.peek()
+# param_4 = obj.empty()
