49e5feb85a83eea942ee3ff1bc02cb841b4ad7b4
Shxrvxshar7 <arsharvesh7@gmail.com>
2026-05-07 17:03:16 +0530

    feat(dp): add house robber solution

---
+++ dp/house_robber.py
@@ -0,0 +1,24 @@
+# Problem: House Robber
+# Link: https://leetcode.com/problems/house-robber/
+# Difficulty: Medium
+# Approach: Dynamic programming
+
+
+class Solution(object):
+    def rob(self, nums):
+        n = len(nums)
+        if n ==1:
+            return nums[0]
+        if n ==2:
+            return max(nums[0],nums[1])
+
+        dp = [0] * n
+        dp[0] = nums[0]
+        dp[1] = max(nums[0],nums[1])
+        
+        for i in range(2,n):
+            dp[i] = max(nums[i] + dp[i-2], dp[i-1])
+
+        return dp[n-1]
+    
+#Time: o(n)
